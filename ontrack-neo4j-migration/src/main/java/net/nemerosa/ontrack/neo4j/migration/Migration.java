d00125b9e9e218f602a4e96244cdd677e094b879
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-02 13:57:24 +0200

    NEO4J - running the migration

--- ontrack-neo4j-migration/src/main/java/net/nemerosa/ontrack/neo4j/migration/Migration.java
+++ ontrack-neo4j-migration/src/main/java/net/nemerosa/ontrack/neo4j/migration/Migration.java
@@ -1,36 +1,12 @@
 package net.nemerosa.ontrack.neo4j.migration;
 
-import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
-import org.springframework.data.neo4j.server.Neo4jServer;
-import org.springframework.data.neo4j.server.RemoteServer;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-public class Migration extends Neo4jConfiguration {
+@Component
+public class Migration {
 
-    @Autowired
-    private MigrationProperties migrationProperties;
+    public void run() {
 
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Migration.class);
-        application.run(args);
-    }
-
-    @Override
-    public Neo4jServer neo4jServer() {
-        return new RemoteServer(
-                migrationProperties.getNeo4j().getUrl(),
-                migrationProperties.getNeo4j().getUsername(),
-                migrationProperties.getNeo4j().getPassword()
-        );
-    }
-
-    @Override
-    public SessionFactory getSessionFactory() {
-        return new SessionFactory("net.nemerosa.ontrack.neo4j.domain");
     }
 
 }
