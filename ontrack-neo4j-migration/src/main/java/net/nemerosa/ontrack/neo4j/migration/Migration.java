7129c0681c34835224535e70017ece6e9dfd9fef
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-04-04 18:38:08 +0200

    NEO4J - project time & user

--- ontrack-neo4j-migration/src/main/java/net/nemerosa/ontrack/neo4j/migration/Migration.java
+++ ontrack-neo4j-migration/src/main/java/net/nemerosa/ontrack/neo4j/migration/Migration.java
@@ -1,16 +1,25 @@
 package net.nemerosa.ontrack.neo4j.migration;
 
 import com.google.common.collect.ImmutableMap;
+import net.nemerosa.ontrack.common.Time;
+import net.nemerosa.ontrack.model.events.EventFactory;
 import net.nemerosa.ontrack.model.structure.Project;
+import net.nemerosa.ontrack.model.structure.Signature;
 import net.nemerosa.ontrack.repository.StructureRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.template.Neo4jTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
 @Component
-public class Migration {
+public class Migration extends NamedParameterJdbcDaoSupport {
 
     private final Logger logger = LoggerFactory.getLogger(Migration.class);
 
@@ -18,9 +27,10 @@ public class Migration {
     private final Neo4jTemplate template;
 
     @Autowired
-    public Migration(StructureRepository structure, Neo4jTemplate template) {
+    public Migration(StructureRepository structure, Neo4jTemplate template, DataSource dataSource) {
         this.structure = structure;
         this.template = template;
+        this.setDataSource(dataSource);
     }
 
     public void run() {
@@ -38,13 +48,41 @@ public class Migration {
 
     private void migrateProject(Project project) {
         logger.info("Migrating project {}...", project.getName());
+
+        Signature projectSignature = getEventSignature("project", project.id());
         template.query(
-                "MERGE (p:Project {name: {name}, description: {description}})",
+                "MERGE (p:Project {name: {name}, description: {description}, createdAt: {createdAt}, createdBy: {createdBy}})",
                 ImmutableMap.<String, Object>builder()
                         .put("name", project.getName())
                         .put("description", safeString(project.getDescription()))
+                        .put("createdAt", Time.toJavaUtilDate(projectSignature.getTime()))
+                        .put("createdBy", projectSignature.getUser().getName())
                         .build()
         );
+    }
+
+    private Signature getEventSignature(String entity, int entityId) {
+        String eventUser;
+        LocalDateTime eventTime;
+        List<Map<String, Object>> results = getNamedParameterJdbcTemplate().queryForList(
+                String.format(
+                        "SELECT * FROM EVENTS WHERE %s = :project AND EVENT_TYPE = :type ORDER BY ID DESC LIMIT 1",
+                        entity
+                ),
+                ImmutableMap.<String, Object>builder()
+                        .put(entity, entityId)
+                        .put("type", EventFactory.NEW_PROJECT.getId())
+                        .build()
+        );
+        if (results.isEmpty()) {
+            eventUser = "import";
+            eventTime = Time.now();
+        } else {
+            Map<String, Object> result = results.get(0);
+            eventUser = (String) result.get("event_user");
+            eventTime = Time.fromStorage((String) result.get("event_time"));
+        }
+        return Signature.of(eventTime, eventUser);
     }
 
     private String safeString(String description) {
